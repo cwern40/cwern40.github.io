83216da8ef0780064ae37c5df29b30e8b9c58801
cwern40 <chris.wernli@hotmail.com>
2020-05-14 18:46:53 -0600

    Trying to fix the contact php code to send out an email

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,23 +1,23 @@
 <?php
-    $name = $_POST['name']
-    $visitor_email = $_POST['email']
-    $message = $_POST['message']
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-    $email_from = 'chris.wernli@hotmail.com'
+    $email_from = 'chris.wernli@hotmail.com';
 
-    $email_subject = 'Portfolio Site Form Submission'
+    $email_subject = "Portfolio Site Form Submission";
 
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                         "User Message: $message.\n";
 
-    $to = 'chris.wernli@hotmail.com'
+    $to = "chris.wernli@hotmail.com";
 
     $headers = "From: $email_from \r\n";
 
     $headers .= "Reply-To: $visitor_email \r\n";
 
-    mail($to,$email_subject,$email_body,$headers)
+    mail($to,$email_subject,$email_body,$headers);
 
     header("Location: index.html");
 
